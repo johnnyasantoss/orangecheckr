f72253042dcb9b21a63989a4bb8f29ecb64b33d1
Johnny Anderson dos Santos <johnnyadsantos@gmail.com>
2023-11-03 03:22:47 +0000

    auth client

--- nostr-client/index.mjs
+++ nostr-client/index.mjs
@@ -1,10 +1,5 @@
 import "websocket-polyfill";
-import {
-  relayInit,
-  finishEvent,
-  generatePrivateKey,
-  getPublicKey,
-} from "nostr-tools";
+import { relayInit, finishEvent, getPublicKey } from "nostr-tools";
 
 const priv = generatePrivateKey();
 const pub = getPublicKey(priv);
@@ -13,20 +8,25 @@ const relayUrl =
   "wss://1337-johnnyasant-satshackora-i5zo9336ya4.ws-us105.gitpod.io";
 const relay = relayInit(relayUrl);
 
-relay.on("auth", (challenge) => {
+relay.on("auth", async (challenge) => {
   console.debug(`Recebeu auth: ${challenge}`);
 
-  const sigEvent = finishEvent({
-    kind: 22242,
-    created_at: Date.now(),
-    content: "",
-    tags: [["relay", , relayUrl], [["challenge", challenge]]],
-  });
+  const sigEvent = finishEvent(
+    {
+      kind: 22242,
+      created_at: Date.now(),
+      content: "",
+      tags: [["relay", , relayUrl], [["challenge", challenge]]],
+    },
+    priv
+  );
 
-  relay.publish(sigEvent);
+  await relay.publish(sigEvent);
+
+  relay.close();
 });
 
-relay.on('notice', console.debug);
+relay.on("notice", console.debug);
 
 relay.on("connect", () => {
   console.log(`connected to ${relay.url}`);
