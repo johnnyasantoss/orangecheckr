47dc5a86c6e3ae8480456bc9b895e5ef3ee1f691
Johnny Anderson dos Santos <johnnyadsantos@gmail.com>
2023-11-03 01:04:35 +0000

    Add auth on client

--- nostr-client/index.mjs
+++ nostr-client/index.mjs
@@ -1,12 +1,39 @@
-import 'websocket-polyfill'
-import { relayInit, finishEvent, generatePrivateKey, getPublicKey } from 'nostr-tools'
+import "websocket-polyfill";
+import {
+  relayInit,
+  finishEvent,
+  generatePrivateKey,
+  getPublicKey,
+} from "nostr-tools";
 
-const relay = relayInit('wss://1337-johnnyasant-satshackora-i5zo9336ya4.ws-us105.gitpod.io')
-relay.on('connect', () => {
-  console.log(`connected to ${relay.url}`)
-})
-relay.on('error', () => {
-  console.log(`failed to connect to ${relay.url}`)
-})
+const priv = generatePrivateKey();
+const pub = getPublicKey(priv);
 
-await relay.connect()
+const relayUrl =
+  "wss://1337-johnnyasant-satshackora-i5zo9336ya4.ws-us105.gitpod.io";
+const relay = relayInit(relayUrl);
+
+relay.on("auth", (challenge) => {
+  console.debug(`Recebeu auth: ${challenge}`);
+
+  const sigEvent = finishEvent({
+    kind: 22242,
+    created_at: Date.now(),
+    content: "",
+    tags: [["relay", , relayUrl], [["challenge", challenge]]],
+  });
+
+  relay.publish(sigEvent);
+});
+
+relay.on('notice', console.debug);
+
+relay.on("connect", () => {
+  console.log(`connected to ${relay.url}`);
+});
+
+relay.on("error", () => {
+  console.log(`failed to connect to ${relay.url}`);
+});
+
+await relay.connect();
