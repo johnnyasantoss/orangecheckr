9d404c64db6c05f12d66d48785fdb14b2fb2739d
Johnny Santos <johnnyadsantos@gmail.com>
2023-11-18 10:07:35 -0300

    Simplify configs

--- proxy/src/config.ts
+++ proxy/src/config.ts
@@ -5,44 +5,40 @@ function missingEnv(key: string): never {
     throw new Error(`Missing environment variable: ${key}`);
 }
 
+function getEnv(key: string, defaultValue?: string) {
+    return process.env[key] || defaultValue || missingEnv(key);
+}
+
 export const config = {
-    envType: process.env["NODE_ENV"] || "development",
-    port: parseInt(process.env["PORT"] || "8080"),
-    invoiceExpirySecs:
-        Number(process.env["INVOICE_EXPIRY_SECS"]) ||
-        missingEnv("INVOICE_EXPIRY_SECS"),
-    workersAmount: Number(process.env["NUM_WORKERS"]),
-    authTimeoutSecs:
-        Number(process.env["AUTH_TIMEOUT_SECS"]) ||
-        missingEnv("AUTH_TIMEOUT_SECS"),
-    proxyUrl: new URL(process.env["PROXY_URI"] || missingEnv("PROXY_URI")),
-    relayUri: process.env["RELAY_URI"] || missingEnv("RELAY_URI"),
-    postingPolicyUrl:
-        process.env["POSTING_POLICY_URL"] || missingEnv("POSTING_POLICY_URL"),
-    botName: process.env["BOT_NAME"] || "Orange Checkr Bot",
-    botAbout:
-        process.env["BOT_ABOUT"] ||
-        "I'm a bot that helps you use Orange Checkr. Made at #SatsHack #2023",
-    botPicture: process.env["BOT_PICTURE"] || "https://i.imgur.com/MBwgeHK.png",
-    botPrivateKey:
-        process.env["BOT_PRIVATE_KEY"] || missingEnv("BOT_PRIVATE_KEY"),
-    managerUser: process.env["USER_MANAGER"] || missingEnv("USER_MANAGER"),
-    adminKey: process.env["ADMIN_KEY"] || missingEnv("ADMIN_KEY"),
-    lnbitsUrl: process.env["LNBITS_URL"] || missingEnv("LNBITS_URL"),
-    collateralRequired:
-        Number(process.env["COLLATERAL_REQUIRED"]) ||
-        missingEnv("COLLATERAL_REQUIRED"),
-    relayId: process.env["RELAY_ID"] || missingEnv("RELAY_ID"),
-    relayInvoiceKey:
-        process.env["RELAY_INVOICE_KEY"] || missingEnv("RELAY_INVOICE_KEY"),
-    policyUrl:
-        process.env["POSTING_POLICY_URL"] || missingEnv("POSTING_POLICY_URL"),
-    filterNipKind: (
-        process.env["FILTER_NIP_KIND"] || missingEnv("FILTER_NIP_KIND")
-    )
+    envType: getEnv("NODE_ENV", "development"),
+    port: parseInt(getEnv("PORT", "8080")),
+    invoiceExpirySecs: Number(getEnv("INVOICE_EXPIRY_SECS")),
+    workersAmount: Number(getEnv("NUM_WORKERS", "0")),
+    authTimeoutSecs: Number(getEnv("AUTH_TIMEOUT_SECS")),
+    proxyUrl: new URL(getEnv("PROXY_URI")),
+    relayUri: getEnv("RELAY_URI"),
+    postingPolicyUrl: getEnv("POSTING_POLICY_URL"),
+    botName: getEnv("BOT_NAME", "Orange Checkr Bot"),
+    botAbout: getEnv(
+        "BOT_ABOUT",
+        "I'm a bot that helps you use Orange Checkr. Made at #SatsHack #2023"
+    ),
+    botPicture: getEnv("BOT_PICTURE", "https://i.imgur.com/MBwgeHK.png"),
+    botPrivateKey: getEnv("BOT_PRIVATE_KEY"),
+    managerUser: getEnv("USER_MANAGER"),
+    adminKey: getEnv("ADMIN_KEY"),
+    lnbitsUrl: getEnv("LNBITS_URL"),
+    collateralRequired: Number(getEnv("COLLATERAL_REQUIRED")),
+    relayId: getEnv("RELAY_ID"),
+    relayInvoiceKey: getEnv("RELAY_INVOICE_KEY"),
+    policyUrl: getEnv("POSTING_POLICY_URL"),
+    checkSpamUrl: getEnv("CHECK_SPAM_URL"),
+    filterNipKind: getEnv("FILTER_NIP_KIND")
         .split(",")
         .map((k) => parseInt(k)),
-    openAiKey: process.env["OPENAI_API_KEY"] || missingEnv("OPENAI_API_KEY"),
+    openAiKey: getEnv("OPENAI_API_KEY"),
+    redisHost: getEnv("REDIS_HOST"),
+    redisPort: getEnv("REDIS_PORT"),
 };
 
 export default config;
