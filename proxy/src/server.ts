4e2ed8bcf2804f75d48a6ac1b36a055ec35f89cf
Johnny Santos <johnnyadsantos@gmail.com>
2023-11-21 07:57:10 -0300

    Avoid crashing when routes have a promise rejection

--- proxy/src/server.ts
+++ proxy/src/server.ts
@@ -1,10 +1,49 @@
 import cors from "cors";
-import express from "express";
+import express, { RequestHandler, Router } from "express";
 import { isDev } from "./config";
 import { reports } from "./reports";
 
+export function reqCatchAsync(fn: RequestHandler): RequestHandler {
+    return (req, res, next) =>
+        Promise.resolve()
+            .then(() => fn(req, res, next))
+            .catch((err) => {
+                console.error(`HTTP ERROR: `, err);
+                return next(err);
+            });
+}
+
+const keys = [
+    "get",
+    "all",
+    "get",
+    "post",
+    "put",
+    "delete",
+    "patch",
+    "options",
+    "head",
+] as const;
+
+function asyncfyRouter<T extends Router>(router: T): T {
+    for (let key of keys) {
+        const method = router[key];
+        router[key] = function asyncRouterWrapper(
+            path: Parameters<typeof method>[0],
+            ...callbacks: any[]
+        ) {
+            return (method as any).call(
+                router,
+                path,
+                ...callbacks.map((cb) => reqCatchAsync(cb))
+            );
+        };
+    }
+    return router;
+}
+
 export function createServerHandler(): Express.Application {
-    const app = express();
+    const app = asyncfyRouter(express());
 
     app.use(cors({ origin: "*" }));
 
@@ -22,14 +61,6 @@ export function createServerHandler(): Express.Application {
     app.post("/webhooks/lnbits/paid/:pubKey", (req, res) => {
         const pubKey = req.params.pubKey;
         return res.status(200).json({ success: true });
-    });
-
-    app.use((_req, _res, next) => {
-        return Promise.resolve()
-            .then(() => next())
-            .catch((err) => {
-                console.error(`HTTP ERROR: `, err);
-            });
     });
 
     app.use((_req, res) => res.json({ notFound: true }).status(404));
